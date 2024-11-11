4c528ab1bc4439cadd8bea512dc024c085055e01
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-11-11 10:41:28 +0800

    added README and better build

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import customtkinter
 import requests
-import argparse
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 def button_callback():
     print("button pressed")
 
 
 app = customtkinter.CTk()
-app.title("my app")
+app.title("Reservation")
 app.geometry("640x360")
 app.grid_columnconfigure(0, weight=1)
 
 
 def check_id(event):
     try:
-        r = requests.get(f'http://127.0.0.1:8000/{str(device_num)}/{MainInput.get()}')
+        r = requests.get(f'{str(config["API_URL"])}/{str(config["DEVICE_NUM"])}/{MainInput.get()}')
         ResultMsg.configure(text=r.json()['message'])
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         ResultMsg.configure(text="網路錯誤")
     MainInput.delete(0, len(MainInput.get()))
 
-
-MainInput = customtkinter.CTkEntry(app, height=140, width=20, font=('Arial', 90), placeholder_text="請刷卡")
+DeviceLabel = customtkinter.CTkLabel(app, height=24, width=20, font=('Arial', 24), text=f'Device No.{str(config["DEVICE_NUM"])}', fg_color="transparent")
+DeviceLabel.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+MainInput = customtkinter.CTkEntry(app, height=90, width=20, font=('Arial', 90), placeholder_text="請刷卡")
 MainInput.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
 MainInput.bind("<Return>", check_id)
 
@@ -30,9 +32,4 @@ ResultMsg = customtkinter.CTkLabel(app, height=140, font=('Arial', 45), text="",
 ResultMsg.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process a device number.')
-    parser.add_argument('device_num', type=int, help='The device number to use')
-    args = parser.parse_args()
-    device_num = args.device_num
-
     app.mainloop()
