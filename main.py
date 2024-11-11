d82685d383fa6c37e7f30f56533aa0199e375e58
Johnny <jmlin0101@gmail.com>
2024-11-11 20:18:17 +0800

    added autofocus and spaces for clarity

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ def check_id(event):
 
 DeviceLabel = customtkinter.CTkLabel(app, height=24, width=20, font=('Arial', 24), text=f'Device No.{str(config["DEVICE_NUM"])}', fg_color="transparent")
 DeviceLabel.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
 MainInput = customtkinter.CTkEntry(app, height=90, width=20, font=('Arial', 90), placeholder_text="請刷卡")
 MainInput.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
 MainInput.bind("<Return>", check_id)
+MainInput.focus_set()
 
 ResultMsg = customtkinter.CTkLabel(app, height=140, font=('Arial', 45), text="", fg_color="transparent")
 ResultMsg.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
