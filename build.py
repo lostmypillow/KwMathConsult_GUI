2644aa054b6f4eb84689772d74d40711a17fb9ba
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-11-11 10:41:28 +0800

    added README and better build

--- build.py
+++ build.py
@@ -1,11 +1,14 @@
 import subprocess
-
+result = subprocess.run("pip show customtkinter", shell=True, capture_output=True, text=True)
+output = result.stdout
+customtkinter_location = str((output.split('\n')[7]).split(' ')[1]) + '/customtkinter;customtkinter/'
+print(customtkinter_location)
 subprocess.run([
     'pyinstaller',
     '--noconfirm',
     '--onedir',
     '--windowed',
     '--add-data',
-    '.venv/Lib/site-packages/customtkinter;customtkinter/',
+    customtkinter_location,
     'main.py'
 ])
