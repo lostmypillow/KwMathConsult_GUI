0b893037f64bde1228f2ce16e67e6a60ab3abe4a
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-11-15 17:03:31 +0800

    rewritten to OOP

--- build.py
+++ build.py
@@ -2,17 +2,20 @@ import subprocess
 import sys
 import os
 import shutil
-result = subprocess.run("pip show customtkinter", shell=True, capture_output=True, text=True)
+result = subprocess.run("pip show customtkinter",
+                        shell=True, capture_output=True, text=True)
 output = result.stdout
-location_string = '/customtkinter:customtkinter/' if sys.platform.startswith('linux') else '/customtkinter;customtkinter/'
-customtkinter_location = str((output.split('\n')[7]).split(' ')[1]) + location_string
+location_string = '/customtkinter:customtkinter/' if sys.platform.startswith(
+    'linux') else '/customtkinter;customtkinter/'
+customtkinter_location = str(
+    (output.split('\n')[7]).split(' ')[1]) + location_string
 subprocess.run([
-    'pyinstaller', '--noconfirm', '--onedir', '--windowed', 
+    'pyinstaller', '--noconfirm', '--onedir', '--windowed',
     '--add-data', customtkinter_location, 'main.py'
 ])
 print('Executable "main" created in dist/main')
-source_env_file = '.env'  
-target_folder = 'dist/main' 
+source_env_file = '.env'
+target_folder = 'dist/main'
 if os.path.exists(source_env_file):
     try:
         shutil.copy(source_env_file, target_folder)
@@ -22,4 +25,3 @@ if os.path.exists(source_env_file):
         print(f"Error copying .env file: {e}")
 else:
     print(".env file not found.")
-
