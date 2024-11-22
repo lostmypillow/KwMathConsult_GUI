63e646350ef4fcdbfca2e12918f6e4bdb8ad39d0
Johnny <jmlin0101@gmail.com>
2024-11-22 20:56:53 +0800

    finished sv ttk integration and Tk rewrite

--- build.py
+++ build.py
@@ -2,26 +2,23 @@ import subprocess
 import sys
 import os
 import shutil
-result = subprocess.run("pip show customtkinter",
-                        shell=True, capture_output=True, text=True)
-output = result.stdout
-location_string = '/customtkinter:customtkinter/' if sys.platform.startswith(
-    'linux') else '/customtkinter;customtkinter/'
-customtkinter_location = str(
-    (output.split('\n')[7]).split(' ')[1]) + location_string
+location_string = 'images/*:images' if sys.platform.startswith(
+    'linux') else 'images/*;images'
 subprocess.run([
-    'pyinstaller', '--noconfirm', '--onedir', '--windowed',
-    '--add-data', customtkinter_location, 'main.py'
+    'pyinstaller', '--noconfirm', '--onefile', '--windowed',
+    '--hidden-import=PIL._tkinter_finder',
+    '--collect-data', 'sv_ttk',
+    '--add-data', location_string,
+    'main.py'
 ])
-print('Executable "main" created in dist/main')
-source_env_file = '.env'
-target_folder = 'dist/main'
-if os.path.exists(source_env_file):
+if os.path.exists('.env'):
     try:
-        shutil.copy(source_env_file, target_folder)
-        print(f'.env file copied to {target_folder}')
-        print('Build Complete. Note that entire main folder is the application, not just the executable')
+        shutil.copy('.env', 'dist')
+        print(f'Copied env file')
+        shutil.copytree('./images', 'dist/images', dirs_exist_ok=True)
+        print('Copied images folder')
     except Exception as e:
-        print(f"Error copying .env file: {e}")
+        print(f"Error copying files: {e}")
 else:
     print(".env file not found.")
+print('Build Complete.')
