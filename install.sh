bfc6a8510f2e6b39285e3960462455019d6beef7
Johnny <jmlin0101@gmail.com>
2024-12-27 18:45:56 +0800

    added pip install

--- install.sh
+++ install.sh
@@ -3,25 +3,25 @@
 # Exit on error
 set -e
 
-echo "[SETUP 1 / 9] Updating system..."
+echo "[SETUP 1 / 10] Updating system..."
 sudo apt update
 
-echo "[SETUP 2 / 9] Ensuring python and pip are installed..."
-sudo apt install python3-venv python3-pip
+echo "[SETUP 2 / 10] Ensuring python and pip are installed..."
+sudo apt install -y python3-venv python3-pip
 
 
 
 # 1. Create and activate virtual environment
 if [ ! -d ".venv" ]; then
-    echo "[SETUP 3 / 9] Creating virtual environment..."
+    echo "[SETUP 3 / 10] Creating virtual environment..."
     python3 -m venv .venv
 fi
 
 # Activate the virtual environment
-echo "[SETUP 4 / 9] Activating virtual environment..."
+echo "[SETUP 4 / 10] Activating virtual environment..."
 source .venv/bin/activate
 
-echo "[SETUP 5 / 9] Checking for Sarasa font in this system..."
+echo "[SETUP 5 / 10] Checking for Sarasa font in this system..."
 # 2. Check if 'Sarasa Fixed TC' font is installed
 if ! fc-list | grep -i "Sarasa Fixed TC"; then
     echo "[SETUP 5.1 / 9] Font 'Sarasa Fixed TC' not found. Installing..."
@@ -50,22 +50,25 @@ if ! fc-list | grep -i "Sarasa Fixed TC"; then
     # Clean up the downloaded archive and extracted files
     rm -rf $FONT_ARCHIVE SarasaFixed
 
-    echo "[SETUP 5.2 / 9] 'Sarasa Fixed TC' font installed successfully."
+    echo "[SETUP 5.2 / 10] 'Sarasa Fixed TC' font installed successfully."
 else
-    echo "[SETUP 5.1 / 9] Font 'Sarasa Fixed TC' is already installed."
+    echo "[SETUP 5.1 / 10] Font 'Sarasa Fixed TC' is already installed."
 fi
 
+echo "[SETUP 6 / 10] Installing from requireemnts.txt"
+pip install -r requirements.txt
+
 # 3. Run python -m build
-echo "[SETUP 6 / 9] Building executable..."
+echo "[SETUP 7 / 10] Building executable..."
 python -m build
 
 # 4. Check and modify .env file in /dist
-echo "[SETUP 7 / 9] Setting up .env file..."
+echo "[SETUP 8 / 10] Setting up .env file..."
 cd dist
 
 # Check if .env file exists, create if it doesn't
 if [ ! -f ".env" ]; then
-    echo "[SETUP 7.1 / 9] .env file not found, creating one..."
+    echo "[SETUP 7.1 / 10] .env file not found, creating one..."
     echo "DEVICE_NUM=1" > .env
     echo "API_URL=http://192.168.2.6:8001" >> .env
 fi
@@ -86,12 +89,12 @@ if [ -n "$NEW_API_URL" ]; then
 fi
 
 # 5. Countdown before running ./main
-echo "[SETUP 8 / 9] Preparing to run executable in"
+echo "[SETUP 9 / 10] Preparing to run executable in"
 for i in {3..1}; do
     echo "$i..."
     sleep 1
 done
 
 # Run ./main (assuming it's an executable or script)
-echo "[SETUP 9 / 9] Running ./main..."
+echo "[SETUP 10 / 10] Running ./main..."
 ./main
